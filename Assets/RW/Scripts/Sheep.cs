7acdb3e7547e049f59ea3603e0c0d117f0a1e6b3
Dali Hoffman (11005824) <HOFFMDE1@student.op.ac.nz>
2023-03-03 13:43:55 +1300

    Feat(SheepDeath): Add Sheep death Heart animation

--- Assets/RW/Scripts/Sheep.cs
+++ Assets/RW/Scripts/Sheep.cs
@@ -16,6 +16,9 @@ public class Sheep : MonoBehaviour
 
     private SheepSpawner sheepSpawner;
 
+    public float heartOffset; 
+public GameObject heartPrefab; 
+
     public void SetSpawner(SheepSpawner spawner)
     {
         sheepSpawner = spawner;
@@ -41,6 +44,10 @@ public class Sheep : MonoBehaviour
         runSpeed = 0;
 
         Destroy(gameObject, gotHayDestroyDelay);
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>();
+        tweenScale.targetScale = 0; 
+        tweenScale.timeToReachTarget = gotHayDestroyDelay;
     }
 
 
